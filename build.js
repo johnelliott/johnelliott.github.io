726ae3b782b3ec6d384c69d992b3bfa8cb0a174f
John Elliott <johnelliott703@gmail.com>
2016-08-12 17:07:18 -0400

    use standard.js style

--- build.js
+++ build.js
@@ -4,23 +4,23 @@ const md = require('marked')
 const posts = require('./posts.json')
 const template = fs.readFileSync(`${__dirname}/template.html`, 'utf8')
 
-const templatePromises = posts.map(post=>{
-  return new Promise(function(resolve, reject) {
+const templatePromises = posts.map(post => {
+  return new Promise(function (resolve, reject) {
     renderPost(post, resolve)
   })
 })
 
-Promise.all(templatePromises).then(function(posts) {
+Promise.all(templatePromises).then(function (posts) {
   // make single page
   fs.writeFileSync(`${__dirname}/index.html`, template.replace('{content}', posts.join('')))
 })
 
-function renderPost(post, callback) {
+function renderPost (post, callback) {
   const markdown = fs.readFileSync(`${__dirname}/posts/${post.markdown}`, 'utf8')
-  md(markdown, (err, content)=>{
+  md(markdown, (err, content) => {
     if (err) throw err
     // make single pages
-    //fs.writeFileSync(`./${file.replace('.md', '.html')}`, template.replace('{content}', content))
+    // fs.writeFileSync(`./${file.replace('.md', '.html')}`, template.replace('{content}', content))
     // add title as anchor link
     const html = `<h1><a href="#${post.title}">${post.title}</a></h1>${content}`
     // pass content along
